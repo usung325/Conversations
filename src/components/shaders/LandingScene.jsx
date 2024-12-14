aaff079048612a1a3126ad22edf4da40ee6b0c98
usung325 <usung325@gmail.com>
2024-12-14 22:57:29 +0900

    test: fixed bottom-0 on landing page to help draggability on mobile

--- src/components/shaders/LandingScene.jsx
+++ src/components/shaders/LandingScene.jsx
@@ -13,7 +13,7 @@ import BLanding from "./BLanding";
 
 export default function LandingScene() {
   return (
-    <div className="w-screen h-screen">
+    <div className="w-screen h-screen fixed bottom-0">
       <Canvas
         className="w-full h-full"
         camera={{ fov: 50, position: [0, 0, 1.4] }}
